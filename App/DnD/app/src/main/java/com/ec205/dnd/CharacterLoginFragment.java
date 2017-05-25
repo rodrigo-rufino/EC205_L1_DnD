32befac13f22f1f17b1f55da11045a26733e75a1
RodrigoRufino <rodrigo.srs.br@gmail.com>
2017-05-25 19:00:04 -0300

    Edição do Documento e conserto de bug de crash no Login

--- App/DnD/app/src/main/java/com/ec205/dnd/CharacterLoginFragment.java
+++ App/DnD/app/src/main/java/com/ec205/dnd/CharacterLoginFragment.java
@@ -75,18 +75,19 @@ public class CharacterLoginFragment extends Fragment {
                 updateUser();
                 System.out.println(dataSnapshot);
 
-                String correctLogin = dataSnapshot.child(login).child("login").getValue().toString();
-                String correctPassword = dataSnapshot.child(login).child("password").getValue().toString();
-
                 if(dataSnapshot.child(login).exists()){
+                    String correctLogin = dataSnapshot.child(login).child("login").getValue().toString();
+                    String correctPassword = dataSnapshot.child(login).child("password").getValue().toString();
                     if (login.compareTo(correctLogin)==0 && password.compareTo(correctPassword)==0){
                         final FragmentTransaction ft = getFragmentManager().beginTransaction();
                         ft.replace(R.id.content_main, new CharacterListFragment().newInstance(login));
                         ft.commit();
-                        Toast.makeText(getActivity(), "Successful Login.", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(getActivity(), "Login realizado com sucesso.", Toast.LENGTH_SHORT).show();
                     } else {
-                        Toast.makeText(getActivity(), "Login and/or password are incorrect.", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(getActivity(), "Login e Senha incorretos.", Toast.LENGTH_SHORT).show();
                     }
+                } else {
+                    Toast.makeText(getActivity(), "Login e Senha incorretos.", Toast.LENGTH_SHORT).show();
                 }
 
 
@@ -107,9 +108,12 @@ public class CharacterLoginFragment extends Fragment {
     public void registerUser(){
         String newLogin = loginEditText.getText().toString();
         String newPassword = passwordEditText.getText().toString();
-
-        mDatabaseRef.child("users").child(newLogin).child("login").setValue(newLogin);
-        mDatabaseRef.child("users").child(newLogin).child("password").setValue(newPassword);
+        if(!newLogin.isEmpty() && !newPassword.isEmpty()){
+            mDatabaseRef.child("users").child(newLogin).child("login").setValue(newLogin);
+            mDatabaseRef.child("users").child(newLogin).child("password").setValue(newPassword);
+        } else {
+            Toast.makeText(getActivity(), "Login e Senha estão vazios.", Toast.LENGTH_SHORT).show();
+        }
     }
 
     public void updateUser(){
